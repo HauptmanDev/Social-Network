7a40629b78eb555bc98c59f72893f6b4f408ca59
HauptmanDev <MainVados@gmail.com>
2019-11-28 22:09:25 +0300

    Updates

---
+++ precache-manifest.6b22f89083858b815dfe94d71c2d6392.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aed75b0358fbe59170dfe4d8b7b0f41",
+    "url": "/Social-Network/index.html"
+  },
+  {
+    "revision": "a9e18d31028e78f69a15",
+    "url": "/Social-Network/static/css/3.50fced24.chunk.css"
+  },
+  {
+    "revision": "5dd7a7102aaa85cb4d93",
+    "url": "/Social-Network/static/css/4.6284f1d5.chunk.css"
+  },
+  {
+    "revision": "87a5f24645c9ed3c276b",
+    "url": "/Social-Network/static/css/main.121f0d30.chunk.css"
+  },
+  {
+    "revision": "6b55a3b18796cb490ea6",
+    "url": "/Social-Network/static/js/2.b9785e89.chunk.js"
+  },
+  {
+    "revision": "a9e18d31028e78f69a15",
+    "url": "/Social-Network/static/js/3.4572adc5.chunk.js"
+  },
+  {
+    "revision": "5dd7a7102aaa85cb4d93",
+    "url": "/Social-Network/static/js/4.4313602f.chunk.js"
+  },
+  {
+    "revision": "87a5f24645c9ed3c276b",
+    "url": "/Social-Network/static/js/main.ee98f739.chunk.js"
+  },
+  {
+    "revision": "c782caa5a86d4767903c",
+    "url": "/Social-Network/static/js/runtime~main.15adf38b.js"
+  },
+  {
+    "revision": "e7f5b6f41255cd77f4f615e42e7078f1",
+    "url": "/Social-Network/static/media/preloader.e7f5b6f4.svg"
+  }
+]);
